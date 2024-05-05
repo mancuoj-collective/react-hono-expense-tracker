aefc316ae7561074a989522ff828950c6334d412
mancuoj <mancuoj@gmail.com>
2024-05-06 00:34:45 +0800

    feat: user profile cache

--- frontend/src/routes/profile.tsx
+++ frontend/src/routes/profile.tsx
@@ -1,23 +1,13 @@
 import { useQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
-import { api } from '~/utils/api'
+import { userQueryOptions } from '~/utils/api'
 
 export const Route = createFileRoute('/profile')({
   component: Profile,
 })
 
-async function getCurrentUser() {
-  const res = await api.me.$get()
-  if (!res.ok) throw new Error('Server error')
-  const data = await res.json()
-  return data
-}
-
 function Profile() {
-  const { isPending, error, data } = useQuery({
-    queryKey: ['get-current-user'],
-    queryFn: getCurrentUser,
-  })
+  const { isPending, error, data } = useQuery(userQueryOptions)
 
   if (isPending) return 'Loading...'
   if (error) return `An error has occurred: ${error.message}`
--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,6 +1,20 @@
+import { queryOptions } from '@tanstack/react-query'
 import { hc } from 'hono/client'
 import type { ApiType } from '~server/app'
 
 const client = hc<ApiType>('/')
 
 export const api = client.api
+
+async function getCurrentUser() {
+  const res = await api.me.$get()
+  if (!res.ok) throw new Error('Server error')
+  const data = await res.json()
+  return data
+}
+
+export const userQueryOptions = queryOptions({
+  queryKey: ['get-current-user'],
+  queryFn: getCurrentUser,
+  staleTime: Infinity,
+})
